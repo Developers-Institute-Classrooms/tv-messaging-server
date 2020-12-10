3c479c14ffdbdb1de2707b03a9c73b9e36757fdb
Nik Ho <studying.mole@gmail.com>
2020-12-10 17:43:47 +1300

    Add user login route and auth middleware.

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -47,9 +47,53 @@ const authRouter = (User) => {
     }
   });
 
-  router.post('/login', (req, res) => {
-    console.log('login');
-    return res.end();
+  router.post('/login', async (req, res) => {
+    try {
+      console.log('login');
+      console.log(req.body);
+
+      const { username, password } = req.body;
+
+      // validate request body properties
+      if (!username || !password) {
+        return res.status(400).json({ error: 'Username and password required!' });
+      }
+  
+      // lookup the user by usernameLower property
+      const usernameLower = username.toLowerCase();
+      console.log(usernameLower);
+
+      // Check the user doesn't exist
+      const existingUser = await User.findOne({ usernameLower });
+      console.log(existingUser);
+  
+      // if the user doesn't exist return a 404 error
+      if (existingUser === null) {
+        return res.status(404).json({ error: 'Username not found!' });
+      }
+  
+      // compare password from the database with recieved password (argon2 verify)
+      if (!await argon2.verify(existingUser.password, password)) {
+        // if passwords don't match throw an error
+        return res.status(401).json({ error: 'Passwords do not match!' });
+      }
+
+      // generate auth token - encode with base64 encoding
+      const token = JSON.stringify({
+        authenticated: true,
+        userId: existingUser._id
+      });
+      console.log(token);
+
+      const encodedToken = Buffer.from(token).toString('base64');
+      console.log(encodedToken);
+  
+      // send token back to the client
+      return res.status(200).json({ token: encodedToken });
+    } catch (e) {
+      console.error(e);
+      return res.status(500).send(e);
+    }
   });
 
   return router;
--- src/routers/messageRouter.js
+++ src/routers/messageRouter.js
@@ -7,11 +7,40 @@ import express from 'express';
  * @property {string} room - Room ID 
  */
 
+const authMiddleware = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ error: 'Authorization header required!' });
+  }
+
+  // check the authorization string is in the correct bearer scheme
+  if (!authorization.startsWith('Bearer ')) {
+    return res.status(401).json({ error: 'Invalid Bearer string!' });
+  }
+
+  // split the token from the Bearer string
+  const encodedToken = authorization.split(' ')[1];
+  console.log(encodedToken);
+
+  // decode the token from base64 encoding
+  const decodedToken = Buffer.from(encodedToken, 'base64').toString('utf8');
+  const token = JSON.parse(decodedToken);
+  console.log(token);
+
+  // check the property of the token - check for authenticated property
+  if (!token.authenticated || !token.userId) {
+    return res.status(401).json({ error: 'Not authenticated!' });
+  }
+
+  next();
+};
+
 const messageRouter = (Message) => {
   const router = express.Router();
 
   // CREATE
-  router.post('/', async (req, res) => {
+  router.post('/', authMiddleware, async (req, res) => {
     try {
       const { message } = req.body;
 
@@ -35,11 +64,13 @@ const messageRouter = (Message) => {
   /**
    * @returns {Message[]}
    */
-  router.get('/', async (req, res) => {
+  router.get('/', authMiddleware, async (req, res) => {
     try {
+      console.log(req.headers);
+
       // get the messages out of the database
       const messages = await Message.find({});
-      console.log(messages);
+      // console.log(messages);
 
       // return messages array to the client
       return res.status(200).json(messages);
@@ -50,7 +81,7 @@ const messageRouter = (Message) => {
   });
 
   // UPDATE
-  router.patch('/:messageId', async (req, res) => {
+  router.patch('/:messageId', authMiddleware, async (req, res) => {
     try {
       const { messageId } = req.params;
 
@@ -68,12 +99,17 @@ const messageRouter = (Message) => {
   });
 
   // DELETE
-  router.delete('/:messageId', async (req, res) => {
+  router.delete('/:messageId', authMiddleware, async (req, res) => {
+    try {
     // 1. extract the messageId from the URL
     const { messageId } = req.params;
     // 2. use the findByIdAndDelete method
     const deleteMessage = await Message.findByIdAndDelete(messageId);
     return res.status(200).json(deleteMessage);
+    } catch (e) {
+      console.error(e);
+      return res.status(500).send(e);
+    }
   });
 
   return router;
